315a8206870bae209eca5d9c6c485e98ca2c9587
wilfriedkt <wilfriedtoure914@gmail.com>
2024-08-06 18:54:07 +0000

    first commit

---
+++ formulaire_connecter.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="formulaire_connecter.css">
+</head>
+<body>
+    <main class="container">
+        <div class="left-section">
+            <div class="left-section-text-container">
+                <h1>La satisfaction commence ici</h1>
+                <ul >
+                  <div> <span>&#10003;</span> <li>Plus de xx catégories d'artisans</li></div>
+                   <div><span>&#10003;</span><li>Mettez en lumière vos services</li></div>
+                   <div><span>&#10003;</span><li>Accès rapide aux acteurs de l'informel</li></div>
+                </ul>
+            </div>
+        </div>
+
+        <div class="right-section">
+          <form action="" method="">
+
+          <div class="container-right-section">
+            <h2>Connectez-vous à votre compte</h2>
+               <span class="qst"> 
+                      <p>Vous n'avez pas de compte? <a href="#">Rejoignez-nous ici</a></p>
+                     
+               </span>
+                <div class="container-bouton-span-gle-email-connect">
+                    <span class="span-gle-email-connect">
+                        <button class="google-btn">Continuer avec Google</button>
+                        <button class="email-btn">Continuer avec l'e-mail</button>
+                    </span>
+                    <p>OU</p>
+                    <button class="facebook-btn">Facebook</button>
+                </div>
+                <p class="terms">
+                    En vous inscrivant, vous acceptez les <a href="#">conditions d'utilisation</a> de DigitArt et recevez occasionnellement des e-mails de notre part. Veuillez lire notre <a href="#">politique de confidentialité</a> pour savoir comment nous utilisons vos données personnelles.
+                </p>
+           </div>
+          </form>
+        </div>
+
+    </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,329 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="footer.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg py-4 py-lg-0 shadow">
+        <div class="container px-4">
+            <img src="./Image/logo.png" alt="">
+            <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#top-navbar" aria-controls="top-navbar">
+                <i class="lni lni-grid-alt"></i>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="top-navbar" aria-labelledby="top-navbarLabel">
+                <!-- Navigation Bar Content -->
+                <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#top-navbar" aria-controls="top-navbar">
+                    <div class="d-flex justify-content-between p-3">
+                        <img src="./Image/logo.png" alt="">
+                        <i class="lni lni-cross-circle"></i>
+                    </div>
+                </button>
+                <ul class="navbar-nav ms-lg-auto p-4 p-lg-0">
+                    <li class="nav-item px-3 px-lg-0 py-1 py-lg-4">
+                        <a class="nav-link" href="#"><span style="color: #fb9818;">Acceuil</span></a>
+                    </li>
+                    <li class="nav-item px-3 px-lg-0 py-1 py-lg-4">
+                        <a class="nav-link" href="#">Artisans</a>
+                    </li>
+                    <li class="nav-item px-3 px-lg-0 py-1 py-lg-4">
+                        <a class="nav-link" href="#">Boutique</a>
+                    </li>
+                    <li class="nav-item px-3 px-lg-0 py-1 py-lg-4">
+                        <a class="nav-link" href="#">A propos</a>
+                    </li>
+                    <li class="nav-item px-3 px-lg-0 py-1 py-lg-4">
+                        <a class="nav-link" href="#">Se connecter</a>
+                    </li>
+                    <li class="nav-item px-3 px-lg-0 py-1 py-lg-4">
+                        <a class="nav-link" href="#">S'inscrire</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+
+
+
+
+
+
+
+
+
+    
+    <section class="hero">
+        <div class="container">
+            <div class="hero-content">
+                <div class="hero-text">
+                    <h1>Trouver les champions de l'informel.</h1>
+                    <ul>
+                        <li>La plus grande plateforme de l'informel en Afrique</li>
+                        <li>Réservez parmi l'élite de l'informel</li>
+                        <li>Mise en relation rapide et facile avec des artisans</li>
+                    </ul>
+                    <form class="search-form">
+                        <input type="text" placeholder="Recherche...">
+                        <button type="submit">Rechercher</button>
+                    </form>
+                </div>
+                <div class="hero-image">
+                    <img src="Image/image_accueil.png" alt="Hero Image" width="500" height="500">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="features">
+        <div class="container">
+            <h1>Videttes du mois</h1>
+            <div class="features-grid">
+                <div class="feature-card">
+                    <img src="Image/ceramiste-3.png" alt="Couturier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/ceramistef.png" alt="Cordonnier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/couturiere-3.png" alt="Menuisier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/cordonnier-4.png" alt="Ebéniste">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/peintre.png" alt="Céramiste">
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="reconnect">
+        <div class="container">
+            <div class="reconnect-content">
+                <div class="reconnect-text">
+                    <h2>Rapprochement client-artisan</h2>
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        <li>Nullam non risus quis nunc tincidunt imperdiet et eu metus.</li>
+                        <li>Vestibulum a massa ut nisi volutpat fermentum et ut nulla.</li>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, ad quas! Dolorem eos eveniet amet voluptates! Aliquam voluptates eveniet in doloremque nostrum omnis facilis eos. Asperiores pariatur ratione laboriosam! Iure?
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque ad corrupti quis accusamus nostrum error, reiciendis, a doloremque rerum, fugit facilis assumenda deleniti eum delectus voluptate at? Soluta, vero laudantium!
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic deserunt cum accusamus inventore, placeat ab. Accusamus obcaecati nihil repudiandae, mollitia delectus provident beatae. Quas aut accusamus provident nulla quisquam nisi?
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia accusamus vel quae voluptate perspiciatis. Adipisci voluptas molestiae provident repellendus perspiciatis totam vitae labore. Cum nisi rem velit dolore, ipsam a!
+                        </p>
+                    </ul>
+                </div>
+                <div class="reconnect-image">
+                    <img src="Image/RAPPROCHEMENT.png" alt="Rapprochement Image">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="cta">
+        <div class="container">
+            <h2>Tentez le meilleur</h2>
+            <button class="cta-btn"><a href="Formulaire2.html">S'inscrire</a></button>
+        </div>
+    </section>
+
+
+   
+
+    
+    <section class="features">
+        <div class="container">
+            <h2>Les métiers</h2>
+            <div class="features-grid">
+                <!-- Ligne 1 -->
+                <div class="feature-card">
+                    <img src="Image/Groupe 288.png" alt="Couturier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 289.png" alt="Cordonnier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 290.png" alt="Menuisier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 291.png" alt="Ebéniste">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 296.png" alt="Céramiste">
+                </div>
+
+                <!-- Ligne 2 -->
+                <div class="feature-card">
+                    <img src="Image/Groupe 296.png" alt="Bijoutier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 293.png" alt="Fleuriste">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 291.png" alt="Électricien">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 295.png" alt="Plombier">
+                </div>
+                <div class="feature-card">
+                    <img src="Image/Groupe 296.png" alt="Jardinier">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="about">
+        <div class="container about-content">
+            <div class="about-text">
+                <h2>Digit'Art : La meilleure plateforme d'artisans</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="about-image">
+                
+            </div>
+        </div>
+    </section>
+
+    <section class="testimonials">
+        <div class="container">
+            <h2>Témoignage</h2>
+            <div class="testimonial">
+                <img src="Image/Témoignage.png" alt="Testimonial Image" width="200" height="200">
+                <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut <br> labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Logos en miniature</title>
+        <style>
+            .logo-container {
+                display: flex;
+                justify-content: space-between;
+                width: 90%; /* Ajustez la largeur selon vos besoins */
+                margin: auto;
+                padding: 20px 0;
+            }
+            .logo-container img {
+                width: 40px; /* Ajustez la taille selon vos besoins */
+                height: auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="logo-container">
+            <p>ils nous font confiance</p>
+            <img src="Image/logo uvci.png" alt="Logo 1" >
+            <img src="Image/logo.png" alt="Logo 2">
+            <img src="Image/Image 1.png" alt="Logo 3">
+            <img src="Image/Image 2.png" alt="Logo 4">
+        </div>
+
+
+
+    
+    <footer>
+        <div class="container footer">
+            <div class="img-container">
+                <img src="Image/logo.png" alt="DigitArt">
+            </div>
+            <div class="row">
+                <div class="col-one-two">
+                    <div class="col-md-6">
+                        <h2>
+                            <span class="border-container">Plan du site</span>
+                        </h2>
+                        <ul>
+                            <li><a class="sous-titre" href="#">À propos</a>
+                                <ul>
+                                    <li><a href="#">Histoire de la plateforme</a></li>
+                                    <li><a href="#">Mission et vision</a></li>
+                                    <li><a href="#">Équipe fondatrice</a></li>
+                                    <li><a href="#">Mentions Légales</a></li>
+                                    <li><a href="#">Politique de Confidentialité</a></li>
+                                    <li><a href="#">Conditions Générales d'Utilisation</a></li>
+                                    <li><a href="#">FAQ</a></li>
+                                    <li ><a class="sous-titre" href="#">Aide et Support</a>
+                                        <ul>
+                                            <li><a href="#">Guide d'utilisation du site</a></li>
+                                            <li><a href="#">Tutoriels vidéo</a></li>
+                                            <li><a href="#">Support en ligne via chat</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 col-two">
+                        <ul>
+                            <li><a class="sous-titre" href="#">Artisans</a>
+                                <ul>
+                                    <li><a href="#">Tous les artisans</a></li>
+                                    <li><a href="#">Catégories</a></li>
+                                    <li><a href="#">Villes ou régions</a></li>
+                                </ul>
+                            </li>
+                            <li ><a class="sous-titre" href="#">Boutiques</a>
+                                <ul>
+                                    <li><a href="#">Offres spéciales</a></li>
+                                    <li><a href="#">Réductions temporaires</a></li>
+                                    <li><a href="#">Nouveautés</a></li>
+                                    <li><a href="#">Produits recommandés</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-6 newsletter">
+                    <h2>
+                        <span class="border-container">Rejoignez notre newsletter</span>
+                    </h2>
+                    <p>Soyez au courant de l'évolution des activités de DigitArt en souscrivant au newsletter</p>
+                    <form>
+                        <input type="email" placeholder="Votre adresse e-mail">
+                        <button class="btn-submit" type="submit">Souscrire</button>
+                    </form>
+                    <ul class="social-icons">
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+</footer>
+<div class="container footer-foot">
+    <span class="spam-foot"></span>
+</div>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
